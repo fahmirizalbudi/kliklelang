e076e23064bfa389c21e33487b50ebf25e986167
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-14 07:40:59 +0700

    feat: wrap content yield with app div

---
+++ server/resources/views/layouts/guest.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/components/text-field.css') }}">
+  <link rel="icon" type="image/svg+xml" href="{{ asset('brand.svg') }}">
+  @stack('styles')
+</head>
+
+<body>
+  <div class="app">@yield('content')</div>
+  @stack('scripts')
+</body>
+
+</html>
