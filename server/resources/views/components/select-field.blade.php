5f69533d31523ddd341de7c45a72d5cf1990d30c
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-16 19:03:48 +0700

    refactor: change the select component to dynamic select library

--- server/resources/views/components/select-field.blade.php
+++ server/resources/views/components/select-field.blade.php
@@ -1,24 +1,16 @@
 @props(['placeholder', 'name', 'id' => null, 'defaultValue' => null])
 
-<div class="form-action-select">
-  <select name="{{ $name }}" id="{{ $id ?? $name }}" class="form-action-select-input" value="{{ $defaultValue }}"
-    required>
-    <option value="" class="placeholder">{{ $placeholder }}</option>
-    {{ $slot }}
-  </select>
-  <span class="form-action-select-icon">
-    <svg class="stroke-current" width="20" height="20" viewBox="0 0 20 20" fill="#fff"
-      xmlns="http://www.w3.org/2000/svg">
-      <path d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396" stroke="currentColor" stroke-width="1.5"
-        stroke-linecap="round" stroke-linejoin="round"></path>
-    </svg>
-  </span>
-</div>
+<select id="{{ $id ?? $name }}" name="{{ $name }}">
+  <option value="" disabled selected hidden style="display: none">{{ $placeholder }}</option>
+  {{ $slot }}
+</select>
 
 @push('scripts')
   <script>
-    document.addEventListener('DOMContentLoaded', () => {
-      document.getElementById('{{ $name ?? $id }}').value = '{{ $defaultValue }}'
-    })
+    new DynamicSelect('#{{ $id ?? $name }}', {
+      name: '{{ $name }}',
+      placeholder: '{{ $placeholder }}',
+      value: '{{ $defaultValue }}',
+    });
   </script>
 @endpush
