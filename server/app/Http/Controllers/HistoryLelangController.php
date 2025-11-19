bf39bd4b0fd445863d493675defe0b7b66606ace
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-19 08:28:27 +0700

    fix: fix history lelang based on officer

--- server/app/Http/Controllers/HistoryLelangController.php
+++ server/app/Http/Controllers/HistoryLelangController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\HistoryLelang;
+use Auth;
 use Illuminate\Http\Request;
 
 class HistoryLelangController extends Controller
 {
     public function __invoke()
     {
+        $petugas = Auth::guard('petugas')->user();
+
         $histories = HistoryLelang::with([
             'masyarakat',
             'lelang.barang'
-        ])->paginate(7);
+        ])->whereHas('lelang', function($query) use ($petugas) {
+            $query->where('id_petugas', $petugas->id_petugas);
+        })->paginate(7);
+
         return view('dashboard.lelang.history.index', compact('histories'));
     }
 }
